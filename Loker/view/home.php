bb64d6d6a886055a90da40b9c78e969aa966b6e7
Zammilzm <madridzammil@gmail.com>
2016-10-18 07:44:18 +0700

    Lowongan Kerja
    Otw

---
+++ Loker/view/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Import Google Icon Font-->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="../asset/css/materialize.css"  media="screen,projection"/>
+	<link rel="stylesheet" type="text/css" href="../asset/css/style.css">
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>HOME</title>
+</head>
+<body>
+	<img src="../image/work5.jpg" id="image-work">
+	<div class="page">
+		<div class="header">
+		<div class="image-header">
+			<li class="logo-kata"><img src="../image/logo.png" class="logo-usaha"></li>
+		</div>
+			<ul class="list-menu-type" >
+				<li class="kata"><a href="daftar.php" id="join-kelas">Daftar</a></li>
+				<li class="kata"><a href="login.php">Login</a></li>
+				<li class="kata"><a href="#">Cari Kerja</a></li>
+				<li class="kata"><a href="#">Perusahaan</a></li>
+				<li class="kata"><a href="#">About</a></li>
+			</ul>
+		</div>
+		<div class="selengkapnya-0">
+			<h3 class="detail-lok">TEMUKAN PEKERJAAN FAVORIT ANDA</h3>
+		</div>
+
+		<div class="selengkapnya">
+			<p class="loker-i">LOKER</p><br>
+		</div>
+
+		<div class="selengkapnya-1">
+			<a href="#" id="selengkapnya-class">Klik Untuk Selengkapnya</a>
+		</div>
+	</div>
+</body>
+</html>
